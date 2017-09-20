6bae3cd180cca46078511b77ac34ec94052c15e0
rgmcwill <rgmcwill@uark.edu>
2017-09-19 20:53:03 -0500

    first commit

--- RegisterServiceDDL.sql
+++ RegisterServiceDDL.sql
@@ -1,7 +1,10 @@
 CREATE EXTENSION "uuid-ossp";
 
-CREATE TABLE product (
+/*this is createing the table -Ross */
+CREATE TABLE employee (
   id uuid NOT NULL,
+  first_name char(10),
+  last_name char(15),
   lookupcode character varying(32) NOT NULL DEFAULT(''),
   count int NOT NULL DEFAULT(0),
   createdon timestamp without time zone NOT NULL DEFAULT now(),
@@ -10,28 +13,17 @@ CREATE TABLE product (
   OIDS=FALSE
 );
 
+/*IDK what this is -Ross*/
 CREATE INDEX ix_product_lookupcode
   ON product
   USING btree
   (lower(lookupcode::text) COLLATE pg_catalog."default");
 
-INSERT INTO product VALUES (
+/*Inserting a test record -Ross*/
+INSERT INTO employee VALUES (
        uuid_generate_v4()
      , 'lookupcode1'
      , 100
      , current_timestamp
 );
 
-INSERT INTO product VALUES (
-       uuid_generate_v4()
-     , 'lookupcode1'
-     , 125
-     , current_timestamp
-);
-
-INSERT INTO product VALUES (
-       uuid_generate_v4()
-     , 'lookupcode3'
-     , 150
-     , current_timestamp
-);
